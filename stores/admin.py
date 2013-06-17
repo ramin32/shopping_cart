85cdfbea625b938820c859bd97739f9e68181e77
Ramin Rakhamimov <ramin32@gmail.com>
2013-06-17 17:07:02 -0400

    Tweaked admin

--- stores/admin.py
+++ stores/admin.py
@@ -1,16 +1,47 @@
 from django.contrib import admin
-from django.contrib.auth.models import User
 from django.contrib.sites.models import Site
 from django.contrib.auth.models import Group
 
 from stores import models
 
-admin.site.unregister(User)
 admin.site.unregister(Group)
 admin.site.unregister(Site)
 
-admin.site.register(models.Store)
-admin.site.register(models.Product)
-admin.site.register(models.Order)
+class ProductInline(admin.TabularInline):
+    model = models.Product
+    extra = 1
 
+class OrderInline(admin.TabularInline):
+    model = models.Order
+    extra = 0
+
+    inlines = [ProductInline]
+
+    def queryset(self, request):
+        qs = super(OrderInline, self).queryset(request)
+        return qs.filter(ordered_on__isnull=False).order_by('ordered_on')
+
+
+class StoreAdmin(admin.ModelAdmin):
+    inlines = [ProductInline, OrderInline]
+    list_filter = ('name', 'product__name')
+    list_display = ('name',)
+
+
+admin.site.register(models.Store, StoreAdmin)
+
+'''
+
+for store in models.Store.objects.all():
+    class OrdersAdmin(admin.ModelAdmin):
+        model = models.Order
+        class Meta:
+            verbose_name = '%s order' % store.name 
+
+        def queryset(self, request):
+            qs = super(OrdersAdmin, self).queryset(request)
+            return qs.filter(ordered_on__isnull=False, store=store).order_by('ordered_on')
+
+    admin.site.register(models.Order, OrdersAdmin)
+'''
 
